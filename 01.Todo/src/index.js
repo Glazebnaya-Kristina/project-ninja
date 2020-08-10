4154018e34010fe9c9de4b64db4969c43c3fce91
tina250594 <tina250594@gmail.com>
2020-08-10 11:06:40 +0300

    Added folders in project

--- 01.Todo/src/index.js
+++ 01.Todo/src/index.js
@@ -1,31 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
+import App from "./components/app";
 
-import './index.sass';
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={ 1 } done={ 3 }></AppHeader>
-      <div className="top-panel d-flex">
-        <SearchPanel/>
-        <ItemStatusFilter/>
-      </div>
-      <TodoList todos={ todoData }></TodoList>
-    </div>
-  );
-}
-
-ReactDOM.render(<App></App>, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
