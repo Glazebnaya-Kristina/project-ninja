4fe03065d6923e3ae4cda09bbb18228091938b9e
tina250594 <tina250594@gmail.com>
2020-08-10 08:52:01 +0300

    Passing props to a child

--- 01.Todo/src/components/todo-list.js
+++ 01.Todo/src/components/todo-list.js
@@ -2,13 +2,10 @@ import React from "react";
 import TodoListItem from "./todo-list-item";
 
 const TodoList = () => {
-
-  const items = ['Drink Coffee', 'Build Awesome App'];
   return (
     <ul>
-      <li><TodoListItem></TodoListItem></li>
-      <li><TodoListItem></TodoListItem></li>
-      <li><TodoListItem></TodoListItem></li>
+      <li><TodoListItem label="Drink Coffee"></TodoListItem></li>
+      <li><TodoListItem label="Build React App" important></TodoListItem></li>
     </ul>
   );
 };
