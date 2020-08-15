0a3f5e9c20ffa34042d90ca204173d8f73a30bec
tina250594 <tina250594@gmail.com>
2020-08-15 17:26:28 +0300

    Added new state

--- 01.Todo/src/components/todo-list-item/todo-list-item.js
+++ 01.Todo/src/components/todo-list-item/todo-list-item.js
@@ -5,24 +5,26 @@ import './todo-list-item.sass';
 export default class TodoListItem extends Component {
 
   state = {
-    done: false
+    done: false,
+    important: false,
   };
 
   onLabelClick = () => {
     this.setState({
-      done: true
+      done: true,
     })
-  }
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    })
+  };
 
   render() {
-    const { label, important = false } = this.props;
+    const { label } = this.props;
 
-    const { done } = this.state;
-
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    const { done, important } = this.state;
 
     let classNames = 'todo-list-item'
 
@@ -30,16 +32,23 @@ export default class TodoListItem extends Component {
       classNames += ' done'
     }
 
+    if(important) {
+      classNames += ' important'
+    }
+
     return (
       <div className={ classNames }>
         <span
           className="todo-list-item__label"
-          style={ style }
           onClick={ this.onLabelClick }
         >
           { label }
         </span>
-        <button type="button" className="btn btn-outline-success btn-sm float-right todo-list-item__button">
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right todo-list-item__button"
+          onClick={ this.onMarkImportant }
+        >
           <i className="fa fa-exclamation"/>
         </button>
         <button type="button" className="btn btn-outline-danger btn-sm float-right todo-list-item__button">
