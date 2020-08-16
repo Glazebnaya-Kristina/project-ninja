ecff486935c2428ec70d6c8f9b7aee8e872d3a50
tina250594 <tina250594@gmail.com>
2020-08-16 11:17:19 +0300

    Added search

--- 01.Todo/src/components/search-panel/search-panel.js
+++ 01.Todo/src/components/search-panel/search-panel.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import './search-panel.sass';
 
-const SearchPanel = () => {
-  const searchText = 'Type here to search';
-  return <input type="text" className="form-control search-input" placeholder={ searchText }/>;
-};
+export default class SearchPanel extends Component {
+  state = {
+    term: ''
+  };
 
-export default SearchPanel;
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({
+      term
+    });
+    this.props.onSearchChange(term);
+  };
+
+  render() {
+    const searchText = 'Type here to search';
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder={ searchText }
+        value={ this.state.term }
+        onChange={ this.onSearchChange }
+      />
+    );
+  };
+};
