f83cdb6753460c58703983b44d78cf57aaf53cac
tina250594 <tina250594@gmail.com>
2020-08-16 11:46:17 +0300

    Added filter

--- 01.Todo/src/components/app/app.js
+++ 01.Todo/src/components/app/app.js
@@ -17,7 +17,8 @@ export default class App extends Component {
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch'),
     ],
-    term: ''
+    term: '',
+    filter: 'all'
   };
 
   createTodoItem(label) {
@@ -87,6 +88,10 @@ export default class App extends Component {
     this.setState({ term });
   };
 
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
   search(items, term) {
     if(term.length === 0) {
       return items;
@@ -98,10 +103,23 @@ export default class App extends Component {
     });
   };
 
-  render() {
-    const { todoData, term } = this.state;
+  filter(items, filter) {
+    switch (filter) {
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter((item) => !item.done);
+      case 'done':
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
+  };
 
-    const visibleItems = this.search(todoData, term);
+  render() {
+    const { todoData, term, filter } = this.state;
+
+    const visibleItems = this.filter(this.search(todoData, term), filter);
     const doneCount = todoData.filter((el) => el.done).length;
 
     const todoCount = todoData.length - doneCount;
@@ -112,7 +130,10 @@ export default class App extends Component {
         <AppHeader toDo={ todoCount } done={ doneCount }/>
         <div className="todo-app__panel d-flex">
           <SearchPanel onSearchChange={ this.onSearchChange }/>
-          <ItemStatusFilter/>
+          <ItemStatusFilter
+            filter={ filter }
+            onFilterChange={ this.onFilterChange }
+          />
         </div>
         <TodoList
           todos={ visibleItems }
