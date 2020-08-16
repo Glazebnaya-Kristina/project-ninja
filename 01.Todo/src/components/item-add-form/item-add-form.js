6742d974752b59f0c52c26647b9a35c2aa341c62
tina250594 <tina250594@gmail.com>
2020-08-16 10:43:24 +0300

    Replace function on class, added listener for input

--- 01.Todo/src/components/item-add-form/item-add-form.js
+++ 01.Todo/src/components/item-add-form/item-add-form.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./item-add-form.sass";
 
-const ItemAddForm = ({ onItemAdd }) => {
-  return (
-    <button className="item-add-form" onClick={ () => onItemAdd('hi') }>Add item</button>
-  );
-}
+export default class ItemAddForm extends Component {
 
-export default ItemAddForm;
+  state = {
+    label: ''
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdd(this.state.label);
+  };
+
+  render() {
+    return (
+      <form className="item-add-form" onSubmit={ this.onSubmit }>
+        <input
+          type="text"
+          className="form-control item-add-form__input"
+          onChange={ this.onLabelChange }
+          placeholder="What need to be done"
+        />
+        <button className="item-add-form__button">Add item</button>
+      </form>
+    );
+  }
+}
